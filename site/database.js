85a5a05e762ebf0be1601f265ecb0328f5428905
Ari Petäjäjärvi <fpvonly@gmail.com>
2018-01-04 01:20:46 +0200

    Mongodb stated working initially

--- site/database.js
+++ site/database.js
@@ -1,3 +1,44 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://admin:MO_mieite8411@localhost:27017';
+var db;
+
+MongoClient.connect(url, function(err, database) {
+  assert.equal(null, err);
+  db = database.db('admin');
+  console.log("Connected successfully to mongodb-server");
+
+  //client.close();
+});
+
+var insert = function(data) {
+  // Get the messages collection
+  const collection = db.collection('messages');
+  collection.insertMany([
+    {
+      "message" : data.message,
+      "user_name": data.user_name,
+      "email": data.email,
+      "ip":  data.ip,
+      "date": data.timestamp
+    }
+  ], function(err, result) {
+      console.log("DB Insert successful");
+  //  assert.equal(err, null);
+  //  assert.equal(3, result.result.n);
+  //  assert.equal(3, result.ops.length);
+  //  callback(result);
+  });
+}
+
+module.exports.insert = insert;
+
+
+
+
+// LEGACY -->
 /*var mysql = require("mysql");
 
 var mysql_con = mysql.createConnection({
