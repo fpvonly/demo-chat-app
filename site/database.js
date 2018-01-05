4bf48fd8641d6a091782890d68eceed2b8b726ac
Ari Petäjäjärvi <fpvonly@gmail.com>
2018-01-06 01:47:40 +0200

    Working on Chat/mongodb integration

--- site/database.js
+++ site/database.js
@@ -1,71 +1,36 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-// Connection URL
-const url = 'mongodb://admin:MO_mieite8411@localhost:27017';
-var db;
+function database() {
+  // Connection URL
+  const url = 'mongodb://admin:MO_mieite8411@localhost:27017';
+  this.db;
+  this.collection;
 
-MongoClient.connect(url, function(err, database) {
-  assert.equal(null, err);
-  db = database.db('admin');
-  console.log("Connected successfully to mongodb-server");
+  MongoClient.connect(url, function(err, mongo) {
+    assert.equal(null, err);
+    this.db = mongo.db('admin');
+    this.collection = this.db.collection('messages');
+    console.log("Connected successfully to mongodb-server");
+    //client.close();
+  }.bind(this));
+};
 
-  //client.close();
-});
-
-var insert = function(data) {
-  // Get the messages collection
-  const collection = db.collection('messages');
-  collection.insertMany([
-    {
-      "message" : data.message,
-      "user_name": data.user_name,
-      "email": data.email,
-      "ip":  data.ip,
-      "date": data.timestamp
-    }
-  ], function(err, result) {
-      console.log("DB Insert successful");
-  //  assert.equal(err, null);
-  //  assert.equal(3, result.result.n);
-  //  assert.equal(3, result.ops.length);
-  //  callback(result);
+database.prototype.find = function(criteria, callback) {
+  // TODO criteria etc
+  this.collection.find({}, function(err, cursor) {
+    assert.equal(null, err);
+    cursor.toArray(callback);
   });
 }
 
-module.exports.insert = insert;
-
-
-
-
-// LEGACY -->
-/*var mysql = require("mysql");
-
-var mysql_con = mysql.createConnection({
-	  host: "localhost",
-	  user: "root",
-	  password: "PE_mieite8411",
-	  database: "nodejs_chat"
-});
-
-mysql_con.connect( function( err ) {
-  if( err )
-  {
-	console.log('Error connecting to DB');
-	return;
-  }
-  console.log('DB Connection established');
-});
-
-this.query = function(sql, callback)
-{
-	//console.log("SQL: " +sql);
-	mysql_con.query( sql, callback);
+database.prototype.insert = function(data) {
+  this.collection.insert(data, function(err, result) {
+      assert.equal(err, null);
+      assert.equal(1, result.insertedCount);
+      //console.log("DB Insert successful", result);
+  }.bind(this));
+  // collection.insertMany([}...
 }
 
-this.escape = function( s )
-{
-	//console.log("VALUE: " +mysql_con.escape( s ) );
-	return mysql_con.escape( s );
-}
-*/
+module.exports = database;
