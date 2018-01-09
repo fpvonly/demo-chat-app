e278520cced7a19b5ad33429a4dae0e70aaa0ddb
Ari Petäjäjärvi <fpvonly@gmail.com>
2018-01-09 19:30:45 +0200

    Fixed routes

--- site/index.jsx
+++ site/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route, IndexRoute, Switch } from 'react-router-dom
 import App from './views/Main.jsx';
 import Index from './views/index.jsx';
 import Contact from './views/Contact.jsx';
+import Info from './views/Info.jsx';
 import NotFound from './views/NotFound.jsx';
 
 
@@ -12,7 +13,8 @@ ReactDOM.render(
     <App>
       <Switch>
         <Route exact path="/" component={Index} />
-        <Route exact path="contact" component={Contact} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/info" component={Info} />
         <Route component={NotFound} />
       </Switch>
     </App>
