ae92b32ad57804b78b5b876189e10797155e635e
Ari Petäjäjärvi <fpvonly@gmail.com>
2018-02-10 19:49:19 +0200

    Updated production build

--- site/src/views/index.jsx
+++ site/src/views/index.jsx
@@ -4,7 +4,6 @@ import {render} from 'react-dom';
 export default class Index extends React.Component {
 
   render() {
-
     // This is a index-page placeholder component for now
     return <div></div>;
   }
