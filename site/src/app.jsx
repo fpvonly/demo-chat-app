afcf77afb88b280e120d0935bfd3a6c62394f6b7
Ari Petäjäjärvi <fpvonly@gmail.com>
2018-02-11 23:25:50 +0200

    views/Index.jsx -> Home.jsx

--- site/src/app.jsx
+++ site/src/app.jsx
@@ -8,7 +8,7 @@ import thunk from 'redux-thunk';
 import reducer from './redux/reducers';
 
 import MainUI from './views/MainUI.jsx';
-import Index from './views/Index.jsx';
+import Home from './views/Home.jsx';
 import Contact from './views/Contact.jsx';
 import Info from './views/Info.jsx';
 import NotFound from './views/NotFound.jsx';
@@ -20,8 +20,8 @@ ReactDOM.render(
     <BrowserRouter>
       <MainUI>
         <Switch>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/admin" component={Index} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/admin" component={Home} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/info" component={Info} />
           <Route component={NotFound} />
--- site/src/views/Home.jsx
+++ site/src/views/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-export default class Index extends React.Component {
+export default class Home extends React.Component {
 
   render() {
     // This is a index-page placeholder component for now
