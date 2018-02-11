09662d6b37521ea2364919c279493a5d34dfbdb3
Ari Petäjäjärvi <fpvonly@gmail.com>
2018-02-11 04:49:41 +0200

    Testing Codeship

--- site/src/test/test-server.js
+++ site/src/test/test-server.js
@@ -9,57 +9,59 @@ function TestServer() {
   let socket;
 
   let server = app.listen(3000, function() {
-    let host = server.address().address;
-    let port = server.address().port;
-    console.log('Test server started');
 
-    socket = new socketserver({
-      httpServer: server
+  });
+
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log('Test server started');
+
+  socket = new socketserver({
+    httpServer: server
+  });
+
+  socket.on('request', function(request) {
+    console.log('Websocket test server - client connected');
+    let connection = request.accept(null, request.origin);
+    count++;
+    connection.id = count;
+    clients[count] = connection;
+
+    connection.sendUTF(JSON.stringify({custom: 'Welcome. Logged in.', _id: 'custom_welcome'}));
+
+    connection.on('message', function(message) {
+      // 0: message
+      // 1: chat name
+      // 2: email
+      let msg_parts = message.utf8Data.split(';');
+      let message_text = msg_parts[0];
+      let chat_name = msg_parts[1];
+      let email = msg_parts[2];
+      let uid = msg_parts[3];
+
+      for(let i in clients) {
+        clients[i].sendUTF(
+          JSON.stringify({
+            message: message_text,
+            timestamp: new Date(),
+            user_name: chat_name,
+            uid: uid,
+            _id: '42r2f34g3'
+          })
+        );
+      }
     });
 
-    socket.on('request', function(request) {
-      console.log('Websocket test server - client connected');
-      let connection = request.accept(null, request.origin);
-      count++;
-      connection.id = count;
-      clients[count] = connection;
-
-      connection.sendUTF(JSON.stringify({custom: 'Welcome. Logged in.', _id: 'custom_welcome'}));
-
-      connection.on('message', function(message) {
-        // 0: message
-        // 1: chat name
-        // 2: email
-        let msg_parts = message.utf8Data.split(';');
-        let message_text = msg_parts[0];
-        let chat_name = msg_parts[1];
-        let email = msg_parts[2];
-        let uid = msg_parts[3];
-
-        for(let i in clients) {
-          clients[i].sendUTF(
-            JSON.stringify({
-              message: message_text,
-              timestamp: new Date(),
-              user_name: chat_name,
-              uid: uid,
-              _id: '42r2f34g3'
-            })
-          );
+    connection.on('close', function(reasonCode, description) {
+      for (let i in clients) {
+        if (i == connection.id) {
+          delete clients[i];
+        }	else {
+          //console.log( i );
         }
-      });
-
-      connection.on('close', function(reasonCode, description) {
-        for (let i in clients) {
-          if (i == connection.id) {
-            delete clients[i];
-          }	else {
-            //console.log( i );
-          }
-        }
-      });
-
+      }
     });
+
   });
 
   app.use(function(req, res, next) {
