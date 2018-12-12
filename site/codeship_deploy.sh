a9968fb1501db65016b72da457f6097c02e697c4
Ari Petäjäjärvi <fpvonly@gmail.com>
2018-12-12 22:44:55 +0200

    Deployment command fix

--- site/codeship_deploy.sh
+++ site/codeship_deploy.sh
@@ -11,4 +11,4 @@ cd ..
 npm run test
 
 # Deployment Commands:
-ssh deploy@146.185.148.22 'git config --global user.email "fpvonly@gmail.com"; git config --global user.name "fpvonly@gmail.com"; cd demo-chat-app/site/; git checkout master; git pull; npm install; npm run prodbuild; git add ./build; git commit -m "Codeship CI build (production) --skip-ci"; git push; cd server/; npm install; sudo npm run foreverrestart;'
+ssh deploy@146.185.148.22 'git config --global user.email "fpvonly@gmail.com"; git config --global user.name "fpvonly@gmail.com"; cd demo-chat-app/site/; git checkout master; git reset --hard HEAD; git pull; npm install; npm run prodbuild; git add ./build; git commit -m "Codeship CI build (production) --skip-ci"; git push; cd server/; npm install; sudo npm run foreverrestart;'
